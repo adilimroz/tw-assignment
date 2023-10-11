b93604b36254d3321912a9420dff01b95375be99
adilimroz <alam.adil12@gmail.com>
2023-10-11 10:19:52 +0530

    adding first test

---
+++ test/specs/android/sampletest.js
@@ -0,0 +1,16 @@
+const testData = require('../../data/testdata.json')
+
+const WelcomeScreen = require('../../screenObjects/android/Welcome.screen')
+const WalletCreationScreen = require('../../screenObjects/android/WalletCreation.screen')
+const Passcode = require('../../screenObjects/android/Passcode.screen')
+const SecretBackup = require('../../screenObjects/android/SecretBackup.screen')
+const Consent = require('../../screenObjects/android/Consent.screen')
+const SecretPhrase = require('../../screenObjects/android/SecretPhrase.screen')
+const Stories = require('../../screenObjects/android/Stories.screen')
+const WalletHome = require('../../screenObjects/android/WalletHome.screen')
+
+describe('End to end tests.', () => {
+    it('End to end successfull user flow.', async() => {
+        console.log("Test");
+    })
+})
